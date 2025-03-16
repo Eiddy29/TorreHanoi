bf5263cdc36a2b1f2f7e826a264bb65e32044f84
Eiddy29 <164039618+Eiddy29@users.noreply.github.com>
2025-03-16 17:00:13 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class TorreDeHanoi {
 
     // Método recursivo para resolver la Torre de Hanoi
-    public static void moverTorres(int n, char origen, char auxiliar, char destino) {
+    public static void moverATorres(int n, char origen, char auxiliar, char destino) {
         if (n == 1) {
             System.out.println("Mover la ficha 1 de " + origen + " a " + destino);
             return;
         }
 
         // Mover (n-1) fichas desde el origen al auxiliar usando el destino
-        moverTorres(n - 1, origen, destino, auxiliar);
+        moverATorres(n - 1, origen, destino, auxiliar);
 
         // Mover la ficha restante desde el origen al destino
         System.out.println("Mover la ficha " + n + " de " + origen + " a " + destino);
 
         // Mover las (n-1) fichas desde el auxiliar al destino usando el origen
-        moverTorres(n - 1, auxiliar, origen, destino);
+        moverATorres(n - 1, auxiliar, origen, destino);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
